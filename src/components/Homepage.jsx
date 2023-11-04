380000f389cfe1cbf709f46fa4ce28cbc23e129b
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-11-04 23:36:40 +0100

    - finishing the homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,37 +14,45 @@ const Homepage = () => {
 
   return (
     <div className=" pl-8">
-      <h2 className="text-2xl font-medium md:mt-10">Global Crypto Stats</h2>
-      <div className="md:grid md:grid-cols-2 md:gap-4 md:mt-3">
+      <h2 className="text-2xl font-medium mt-10">Global Crypto Stats</h2>
+      <div className="md:grid flex flex-wrap gap-x-5 gap-y-4 items-center md:grid-cols-2 md:gap-4 md:mt-3">
         <div>
           <div className="text text-gray-400">Total Cryptocurrencies</div>
-          <div className="text-2xl">{globalStats.total}</div>
+          <div className=" text-lg md:text-2xl">{globalStats.total}</div>
         </div>
         <div>
           <div className="text text-gray-400">Total Exchanges</div>
-          <div className="text-2xl">{millify(globalStats.totalExchanges)}</div>
+          <div className=" text-lg md:text-2xl">
+            {millify(globalStats.totalExchanges)}
+          </div>
         </div>
         <div>
           <div className="text text-gray-400">Total Market Cap</div>
-          <div className="text-2xl">{millify(globalStats.totalMarketCap)}</div>
+          <div className=" text-lg md:text-2xl">
+            {millify(globalStats.totalMarketCap)}
+          </div>
         </div>
         <div>
           <div className="text text-gray-400">Total 24Hr Volume</div>
-          <div className="text-2xl">{millify(globalStats.total24hVolume)}</div>
+          <div className=" text-lg md:text-2xl">
+            {millify(globalStats.total24hVolume)}
+          </div>
         </div>
         <div>
           <div className="text text-gray-400">Total Markets</div>
-          <div className="text-2xl">{millify(globalStats.totalMarkets)}</div>
+          <div className=" text-lg md:text-2xl">
+            {millify(globalStats.totalMarkets)}
+          </div>
         </div>
       </div>
 
-      <div className="md:mt-8 flex md:items-center md:justify-between mx-3">
-        <h2 className="text-2xl font-semibold">
+      <div className="md:mt-8 mt-6 flex items-center justify-center gap-8 md:justify-between mx-0 md:mx-3">
+        <h2 className=" text-lg md:text-2xl font-semibold">
           Top 10 Cryptocurrencies in the world
         </h2>
-        <div className="flex items-center justify-between">
-          <h3 className="text-xl">
-            <Link to="/cryptocurrencies" className="text-blue-500">
+        <div className="flex items-center ">
+          <h3 className="  md:text-xl ">
+            <Link to="/cryptocurrencies" className="text-purple-800">
               Show More
             </Link>
           </h3>
@@ -52,11 +60,11 @@ const Homepage = () => {
       </div>
       <Cryptocurrencies simplified />
 
-      <div className="md:mt-8 md:space-y-4">
-        <h2 className="text-3xl font-bold">Latest Crypto News</h2>
+      <div className="flex items-center justify-between mx-2 mt-4 md:mt-8 md:space-y-4">
+        <h2 className="text-lg font-bold">Latest Crypto News</h2>
         <div className="flex items-center justify-between">
-          <h3 className="text-2xl">
-            <Link to="/news" className="text-blue-500">
+          <h3 className="">
+            <Link to="/news" className="text-purple-300">
               Show More
             </Link>
           </h3>
