6f408c7730e67a01be43b66356b7279fd43e8481
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-11-08 15:06:04 +0100

    Fixed issues on crypto currency page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        200: "200px",
+        300: "300px",
+        400: "400px",
+        500: "500px",
+        600: "600px",
+        700: "700px",
+      },
+    },
   },
   plugins: [],
 };
